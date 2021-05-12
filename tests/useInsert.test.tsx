d0b1b2663619d3810f7ed18eca7462f45870fa17
joshuadutton <joshuadutton@gmail.com>
2021-05-12 15:07:37 -0600

    Adds mutation tests for insert

---
+++ tests/useInsert.test.tsx
@@ -0,0 +1,43 @@
+import { waitFor } from "@testing-library/react";
+import { renderHook, act } from "@testing-library/react-hooks/dom";
+
+import HasuraConfig from "./TestHasuraConfig";
+import { useReactGraphql } from "../src/hooks/useReactGraphql";
+import { wrapperWithResultValue } from "./urqlTestUtils";
+
+const resultValue = { id: '123' };
+
+describe("useInsert", () => {
+
+  it("sets up an insert", async () => {
+    const wrapper = wrapperWithResultValue(resultValue, 'mutation');
+
+    const { result } = renderHook(() => {
+      const reactGraphql = useReactGraphql(HasuraConfig.groups);
+      return reactGraphql.useInsert({});
+    }, { wrapper });
+    expect(result.current.resultItem).toBeUndefined();
+
+    await waitFor(() => result.current.executeMutation());
+    expect(result.current.resultItem.id).toBe(resultValue.id);
+  });
+
+  it("inserts with updating variables", async () => {
+    const wrapper = wrapperWithResultValue(resultValue, 'mutation');
+
+    const { result } = renderHook(() => {
+      const reactGraphql = useReactGraphql(HasuraConfig.groups);
+      return reactGraphql.useInsert({ initialVariables: { id: "456" }});
+    }, { wrapper });
+    expect(result.current.resultItem).toBeUndefined();
+
+    act(() => {
+      result.current.setVariable('postId', '897');
+    });
+
+    await waitFor(() => result.current.executeMutation({ groupId: "123" }))
+    expect(result.current.resultItem.id).toBe("456");
+    expect(result.current.resultItem.postId).toBe('897');
+    expect(result.current.resultItem.groupId).toBe("123");
+  });
+});
