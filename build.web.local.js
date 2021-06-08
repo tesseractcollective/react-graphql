2d949d8cbb875754254b8377324921f533ac5519
Jeremy R <jeremy@wavelengthdev.com>
2021-06-08 15:45:17 -0600

    web build fixes

--- build.web.local.js
+++ build.web.local.js
@@ -10,7 +10,7 @@ require('esbuild')
     bundle: true,
     target: ['es2017'],
     format: 'esm',
-    outfile: './dist/src/index.js',
+    outfile: '../purco/node_modules/@tesseractcollective/react-graphql/dist/src/index.js',
     tsconfig: './tsconfig-web.json',
     define: { 'process.env.NODE_ENV': '"production"', __DEV__: false, global: 'window' },
     resolveExtensions: ['.web.tsx', '.web.ts', '.web.jsx', '.web.js', '.tsx', '.ts', '.jsx', '.js'], //This takes into account priority, so it will resolve a .web.tsx of the same file name before a .tsx if it exists
