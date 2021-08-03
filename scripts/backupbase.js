864a223c940d95af9f544886e91bed03cdb2a1cc
Jeremy R <jeremy@wavelengthdev.com>
2021-08-03 16:28:57 -0600

    v0.8.14:pre - re setup builds and dependencies

--- scripts/backupbase.js
+++ scripts/backupbase.js
@@ -6,6 +6,10 @@ const baseEnvs = require('../package.json');
 delete baseEnvs.name;
 delete baseEnvs.peerDependencies;
 delete baseEnvs.dependencies;
+delete baseEnvs.main;
+delete baseEnvs.types;
+delete baseEnvs.exports;
+delete baseEnvs.module;
 //copy the json inside the env.json file
 fs.writeFileSync('package.base.json', JSON.stringify(baseEnvs, undefined, 2));
 
